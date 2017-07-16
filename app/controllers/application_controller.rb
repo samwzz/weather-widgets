fab390ffa5aae49c195d0b9f59cc1faec45df1f5
Sam Wang <sam.zz.wang@gmail.com>
2017-07-15 17:30:12 -0700

    Implement auth backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    @current_user = user
+    session[:session_token] = user.reset_token!
+  end
+
+  def logout
+    @current_user.try(:reset_token!)
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def require_logged_in
+    render json: { base: ['invalid credentials'] }, status: 401 if !current_user
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && user.valid_password?(password)
+    nil
+  end
+
+  def reset_token!
+    self.session_token = generate_session_token!
+    self.save
+    self.session_token
+  end
+
+  private
+
+  def generate_session_token!
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token!
+  end
+end
